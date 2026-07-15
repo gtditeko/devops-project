94a529b1931a3035723557ea2259372f83c5a7c0
Gabriel <12709037+gtditeko@users.noreply.github.com>
2026-07-15 21:22:58 +0200

    Add cloudfront distribution to the static s3 website

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,3 +54,45 @@ resource "aws_s3_bucket_policy" "website" {
 
   depends_on = [aws_s3_bucket_public_access_block.website]
 }
+
+resource "aws_cloudfront_origin_access_control" "website" {
+  name                              = "website-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "s3-website-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.website.id
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "s3-website-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
