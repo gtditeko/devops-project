206018bc69ec2dac7b4a3c9a4d8d57a44a69738f
Gabriel <12709037+gtditeko@users.noreply.github.com>
2026-07-19 06:57:16 +0200

    Test terraform CI workflow trigger

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket" "website" {
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
-  bucket = aws_s3_bucket.website.id
+    bucket = aws_s3_bucket.website.id
 
   index_document {
     suffix = "index.html"
